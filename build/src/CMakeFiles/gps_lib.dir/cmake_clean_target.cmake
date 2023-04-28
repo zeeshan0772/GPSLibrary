2e3f2290de27e1b0cbd798c9e8f2353b13075dba
zeeshan_ansar <i190772@nu.edu.pk>
2023-04-29 00:15:51 +0500

    updated unit tests

--- build/src/CMakeFiles/gps_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "../lib/libgps_lib.a"
-)
--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zeeshan/Desktop/GPS Library/test
-# Build directory: /home/zeeshan/Desktop/GPS Library/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/zeeshan/Desktop/GPS Library/build/test/unit_tests[1]_include.cmake")
-subdirs("../_deps/gtest-build")
